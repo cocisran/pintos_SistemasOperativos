e59815ea84c5d2a4bc55e4df53d4d7f7f7edb4e1
cocisran <fcoemmdmm@ciencias.unam.mx>
2022-05-16 08:04:37 -0500

    cambio de manejador de alojador de memoria

--- userprog/process.c
+++ userprog/process.c
@@ -537,7 +537,7 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
       size_t page_zero_bytes = PGSIZE - page_read_bytes;
 
       /* Get a page of memory. */
-      uint8_t *kpage = palloc_get_page (PAL_USER);
+      uint8_t *kpage = frame_get_page(upage);
       if (kpage == NULL)
         return false;
 
