90db27fc1e8aef5e5fe70be24beb8f1e3681b1c4
cocisran <fcoemmdmm@ciencias.unam.mx>
2022-05-16 08:03:52 -0500

    maquetacion de la solucion

--- vm/frame.c
+++ vm/frame.c
@@ -1,12 +1,80 @@
+/*Este archivo sirve para mantener el nuevo mapeo de la memoria
+virtual de pintos*/
 #include "vm/frame.h"
 
 #include <stdio.h>
+#include <stdlib.h>
+#include "threads/palloc.h"
+#include "threads/thread.h"
+#include "threads/synch.h"
+#include "threads/malloc.h"
+#include "userprog/pagedir.h"
 
-struct list frames; /*Lista de los marcos de cada proceso*/
+static struct list frames_list; /*Lista de los marcos de cada proceso*/
+static struct lock frame_list_lock; /*Semaforo para limitar la edicion de la lista de frames*/
+
+static struct frame_entry* choose_victim_frame(void);
 
 /*Inicia la lista global de marcos*/
 void 
 frame_init(void)
 {
-    list_init(&frames);
+    list_init(&frames_list);
+    lock_init(&frame_list_lock);
+}
+
+
+/*Aloja una pagina de proceso dentro de un margo*/
+void* 
+frame_get_page(uint8_t *upage)
+{
+    /*Obtener un marco para alojar la pagina*/
+    uint8_t *kpage = palloc_get_page (PAL_USER);
+    /*Si el no hay un marco disponible recurrir al swap*/
+
+    /*Una vez que fue exitoso el alojamiento registrarlo en la lista*/
+    add_frame(upage,kpage);
+    return kpage;
+}
+
+
+/*Agrega la informacion administrativa del mapeo  a la memoria 
+virtual de este proceso a la tabla de paginacion*/
+void
+add_frame(uint8_t *upage, uint8_t *kpage)
+{
+    lock_acquire (&frame_list_lock);
+    struct  thread *t = thread_current();
+    struct frame_entry* f = (struct frame_entry*)malloc(sizeof(struct frame_entry));
+    f->t = t;
+    f->upage = upage;
+    f->kpage = kpage;
+    list_push_back (&frames_list, &f->elem);
+    lock_release (&frame_list_lock);
+}
+
+
+/*Libera espacio en la memoria principal realizando swaping.
+Debe ser llamada desde un contexto safe thread, pues pueden haber errores
+si es accesada de forma concurrente*/
+void  
+free_space()
+{
+/*Obtener pagina victima*/
+    struct frame_entry* victim = choose_victim_frame();
+
+    uint32_t *pd = victim->t->pagedir;
+    uint8_t *upage = victim->upage;
+
+/*Respaldar pagina en memoria secundaria*/
+
+/*Libera la pagina*/
+    pagedir_clear_page (pd,upage); /*pagedir libera el marco?*/
+}
+
+/*Funcion que escoge un marco victima para liberar a memoria secundaria*/
+struct frame_entry* 
+choose_victim_frame()
+{
+    return list_entry (list_pop_front (&frames_list), struct frame_entry, elem);
 }
--- vm/frame.h
+++ vm/frame.h
@@ -2,7 +2,6 @@
 
 #include "threads/thread.h"
 #include <debug.h>
-#include <stdlib.h>
 #include <list.h>
 
 /*Este archivo maneja la reservacion de paginas virtuales para los procesos de usuario*/
@@ -10,11 +9,21 @@
 struct frame_entry
 {
     struct thread *t;  /*thread al cual esta asociado el marco*/
-    uint8_t* upage;   /**/
-    uint8_t* kpage;
+    uint8_t* upage;   /*Pagina virtual */
+    uint8_t* kpage;   /*Marco que aloja la pagina*/
     struct list_elem elem;  /*Para poder construir una lista*/
 };
 
 
 /*Funcion que inicializa la lista de marcos*/
 void frame_init(void);
+
+/*Funcion que permite alojar una pagina dentro de un marco*/
+void*  frame_get_page(uint8_t *);
+
+/*Agrega una entrada a la tabla de paginacion*/
+void add_frame(uint8_t *, uint8_t *);
+
+/*Libera un marco de la memoria principal mandandolo a almacenamiento
+secundario*/
+void   free_space(void);
